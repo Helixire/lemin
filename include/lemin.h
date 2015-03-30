8085406f2b14ee7674589863c2879e85d2461d93
Romain Baudin <romain.baudin@epitech.eu>
2015-03-30 14:10:00 +0200

    izi

---
+++ include/lemin.h
@@ -0,0 +1,14 @@
+/*
+** lemin.h for  in /home/baudin_r/rendu/Module/C/Lem_in/CPE_2014_lemin/include
+**
+** Made by Romain Baudin
+** Login   <baudin_r@epitech.net>
+**
+** Started on  Mon Mar 30 14:03:39 2015 Romain Baudin
+** Last update Mon Mar 30 14:05:05 2015 Romain Baudin
+*/
+
+#ifndef LEMIN_H_
+# define LEMIN_H_
+
+#endif	/* !LEMIN_H_ */
---
+++ sources/main.c
@@ -0,0 +1,9 @@
+/*
+** main.c for  in /home/baudin_r/rendu/Module/C/Lem_in/CPE_2014_lemin/sources
+**
+** Made by Romain Baudin
+** Login   <baudin_r@epitech.net>
+**
+** Started on  Mon Mar 30 14:02:43 2015 Romain Baudin
+** Last update Mon Mar 30 14:02:44 2015 Romain Baudin
+*/
